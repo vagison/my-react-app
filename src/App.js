56d4b5e397a7ac9661dbc3b9c3483f32a98e1737
vagison <vagison@gmail.com>
2022-06-23 14:05:47 +0400

    Piavka's Piavka

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit's Piavka <code>src/App.js</code> and save to reload.
+          Edit's Piavka's Piavka <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"

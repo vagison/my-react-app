5b17dac0f8bccd515dcb39682c85d33fdee0db90
vagison <vagison@gmail.com>
2022-06-25 23:15:48 +0400

    Updating script.sh and package-lock.json also Edit's Shvabra's Pastel

--- script.sh
+++ script.sh
@@ -1,3 +1,10 @@
+# adding env variables from .env
+set -a
+. ./.env
+set +a
+
+printenv
+
 # installing dependencies
 echo "##### Installing dependencies"
 npm install
@@ -11,37 +18,42 @@ if [ $? -eq 0 ]; then # if dependencies installation went successfull
         # switching to versions containing folder
         echo "##### Switching to versions containing folder"
         cd ../
-        # creating tmp folder
-        echo "##### Creating tmp folder"
-        mkdir tmp
+        # creating temporary folder
+        echo "##### Creating temporary folder"
+        mkdir $TEMP_FOLDER
         # switching to the repo folder
         echo "##### Switching to the repo folder"
-        cd ./my-react-app/
-        # copying build files into tmp folder
-        # cp -r build package.json package-lock.json README.md ../tmp
-        echo "##### Copying build files into tmp folder"
-        cp -r build ../tmp
+        cd $REPO_FOLDER
+        # copying build files into temporary folder
+        echo "##### Copying build files into temporary folder"
+        # sleep 30
+        cp -r build ../$TEMP_FOLDER
         # switching to versions containing folder
         echo "##### Switching to versions containing folder"
         cd ../
         # killing running app
-        echo "##### Killing the running app"
-        fuser -k 3000/tcp
-        # changing running folder's name into dummy
-        echo "##### Changing running folder's name into dummy"
-        mv ./running/ ./dummy/
-        # changing tmp folder's name into running
-        echo "##### Changing tmp folder's name into running"
-        mv ./tmp/ ./running/
-        # removing dummy folder
-        echo "##### Removing dummy folder"
-        rm -rf ./dummy/
+        echo "##### Killing the running app on port $PORT"
+        fuser -k $PORT/tcp
+        # sleep 5
+        # changing running folder's name into old
+        echo "##### Changing running folder's name into old"
+        mv $RUNNING_FOLDER $OLD_FOLDER
+        # changing temporary folder's name into running
+        echo "##### Changing temporary folder's name into running"
+        mv $TEMP_FOLDER $RUNNING_FOLDER
         # switching to running folder
         echo "##### Switching to running folder"
-        cd ./my-react-app/
+        cd $RUNNING_FOLDER
         # running the app
-        echo "##### Running the app"
-        serve -s build
+        echo "##### Running the app on port $PORT"
+        serve -s build -l $PORT &>filename.log &
+        echo "##### Enjoy the app!"
+        # switching to versions containing folder
+        echo "##### Switching to versions containing folder"
+        cd ../
+        # removing old folder
+        echo "##### Removing old folder"
+        rm -rf $OLD_FOLDER
     else
         echo "##### There was an error during build phase"
     fi
--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit's Piavka's Paste <code>src/App.js</code> and save to reload.
+          Edit's Shvabra's Pastel!!! <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"

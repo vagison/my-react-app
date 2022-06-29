7eb2ef4b8074f039b4d22cf54efde750cc005820
vagison <vagison@gmail.com>
2022-06-29 19:23:51 +0400

    test tezt

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Test 123! <code>src/App.js</code> and save to reload.
+          Test Test Test! <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"

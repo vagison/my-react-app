37ee53efa95d758d9c0e8a253ec91bfff55496a1
unknown <vagison@gmail.com>
2022-06-29 23:34:12 +0400

    Django Unchained

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Chunga Changa <code>src/App.js</code> and save to reload.
+          Django Unchained <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"

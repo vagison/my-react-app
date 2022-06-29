bcebbea0e5f9ffad71af922aacf0d18f3182eb90
vagison <vagison@gmail.com>
2022-06-29 19:06:25 +0400

    Minnnnor change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Test 222! <code>src/App.js</code> and save to reload.
+          Test 123! <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"

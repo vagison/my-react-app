e4d4ccaf8492dd7cadf9e154d29b29c526668f69
vagison <vagison@gmail.com>
2022-06-30 16:31:23 +0400

    Blabla

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Deployment Test 123 <code>src/App.js</code> and save to reload.
+          Blabla <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"

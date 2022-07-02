d7ec97271fab3afe2d4e011fbc620acb3ad86a2e
vagison <vagison@gmail.com>
2022-07-02 12:33:15 +0400

    Cauldron i!

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Cauldron i tochka! <code>src/App.js</code> and save to reload.
+          Cauldron i! <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"

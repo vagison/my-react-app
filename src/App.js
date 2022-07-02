c196a83190dc7f906568f0b6cbfa9997193159ae
vagison <vagison@gmail.com>
2022-07-02 18:36:30 +0400

    Test change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Cauldron i! <code>src/App.js</code> and save to reload.
+          Cauldron ev verj! <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
